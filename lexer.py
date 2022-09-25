c6071c8ae93d25e6bb59d05402d94b1ed3fd69be
iaal96 <ivan.anguiano17@hotmail.com>
2022-09-25 02:40:04 -0500

    Lexer

---
+++ lexer.py
@@ -0,0 +1,97 @@
+import sys
+import ply.lex as lex 
+import ply.yacc as yacc
+
+reserved = {
+    'programa' : 'PROGRAMA',
+    'si' : 'COND_SI',
+    'entonces':'ENTONCES',
+    'sino' : 'COND_SINO',
+    'mientras':'MIENTRAS',
+    'para':'PARA',
+    'clase':'CLASE',
+    'imprime' : 'IMPRIME',
+    'funcion':'FUNCION',
+    'var' : 'VAR',
+    'int' : 'INT',
+    'float':'FLOAT',
+    'string':'STRING',
+    'bool':'BOOL',
+    'char': 'CHAR',
+    'verdadero':'VERDADERO',
+    'falso':'FALSO',
+    'regresa':'REGRESA',
+    'fin':'FIN',
+}
+
+#tokens iniciales
+tokens = [
+    'MAS',
+    'MENOS',
+    'POR',
+    'DIVIDE',
+    'ID',
+    'IGUAL',
+    'MAYOR_QUE',
+    'MENOR_QUE',
+    'DIFERENTE_A',
+    'LPARENT',
+    'RPARENT',
+    'LBRACE',
+    'RBRACE',
+    'CTEINT',
+    'CTEFLOAT',
+    'COMA',
+    'SEMICOLON',
+    'COLON',
+    'STRING',
+    'PUNTO',
+    'CHAR'
+] +list(reserved.values())
+
+#expresiones regulares para tokens
+t_MAS = r'\+'
+t_MENOS = r'-'
+t_POR = r'\*'
+t_DIVIDE = r'/'
+t_IGUAL = r'\='
+t_DIFERENTE_A = r'<>'
+t_MAYOR_QUE = r'>'
+t_MENOR_QUE = r'<'
+t_LPARENT = r'\('
+t_RPARENT = r'\)'
+t_LBRACE = r'\{'
+t_RBRACE = r'\}'
+t_COMA = r','
+t_PUNTO = r'\.'
+t_SEMICOLON = r';'
+t_COLON = r':'
+t_CTEINT = r'[0-9]+'
+t_CTEFLOAT = r'[0-9]+\.[0-9]+'
+t_STRING = r'"([^\\"\n]+|\\.)*"'
+t_CHAR = r"\'.\'"
+
+#agregar palabras reservadas a los tokens
+tokens = tokens + list(reserved.values())
+
+#checa palabras reservadas
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
+    return t
+
+#definir regla
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+#ignorar espacios en blanco del archivo
+t_ignore = r' '
+
+#error 
+def t_error(t):
+    print("Error en caracter '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# construir analizador lexico
+lex.lex()
---
+++ parser.py
@@ -0,0 +1,132 @@
+import ply.yacc as yacc
+from lexer import tokens
+
+#Vector Polaco
+VectorPolaco = []
+OpStack = []
+VarsStack=[]
+
+#Cuadruplos
+PilaO = []
+PilaSaltos = []
+PilaTipos = []
+Cuadruplos = []
+
+#reglas gramaticales
+def p_programa(p):
+    ''' programa : PROGRAMA ID SEMICOLON bloque
+                | PROGRAMA ID SEMICOLON vars bloque
+    '''
+
+def p_vars(p):
+    ''' vars : VAR vars2
+    '''
+
+def p_vars2(p):
+    ''' vars2 : vars3 COLON tipo SEMICOLON vars2
+                | vars3 COLON tipo SEMICOLON 
+    '''
+
+def p_vars3(p):
+    ''' vars3 : ID
+            | ID COMA vars3
+    '''
+
+def p_tipo(p):
+    ''' tipo : INT
+            | FLOAT
+    '''
+
+def p_bloque(p):
+    ''' bloque : LBRACE estatuto2 RBRACE
+                | LBRACE RBRACE
+    '''
+
+def p_estatuto2(p):
+    ''' estatuto2 : estatuto
+                | estatuto2 estatuto 
+    '''
+
+def p_estatuto(p):
+    ''' estatuto : asignacion
+                | condicion
+                | escritura
+    '''
+def p_asignacion(p):
+    ''' asignacion : ID IGUAL expresion SEMICOLON
+    '''
+
+def p_condicion(p):
+    '''condicion : COND_SI LPARENT expresion RPARENT bloque SEMICOLON
+                | COND_SI LPARENT expresion RPARENT bloque COND_SINO bloque SEMICOLON
+    '''
+
+def p_escritura(p):
+    ''' escritura : IMPRIME LPARENT escrituraGrammar RPARENT SEMICOLON
+    '''
+
+def p_escrituraGrammar(p):
+    ''' escrituraGrammar : expresion
+                        | expresion COMA escrituraGrammar
+                        | varcte
+                        | varcte COMA escrituraGrammar
+    '''
+
+def p_expresion(p):
+    ''' expresion : exp
+                | exp MAYOR_QUE exp
+                | exp MENOR_QUE exp
+                | exp DIFERENTE_A exp
+    '''
+
+def p_exp(p):
+    ''' exp : termino
+            | termino exp2
+    '''
+
+def p_exp2(p):
+    '''exp2 : MAS exp
+            | MENOS exp
+    '''
+
+def p_termino(p):
+    ''' termino : factor
+            | factor termino2
+    '''
+
+def p_termino2(p):
+    ''' termino2 : POR termino
+                | DIVIDE termino
+    '''
+
+def p_factor(p):
+    ''' factor : LPARENT expresion RPARENT
+            | MAS varcte
+            | MENOS varcte
+            | varcte
+    '''
+
+def p_varcte(p):
+    ''' varcte : ID
+            | CTEFLOAT
+            | CTEINT
+            | STRING
+    '''
+def p_error(p):
+    print("Error de sintaxis: '%s'" % p.value)
+    exit()
+
+
+#crear parser
+parser = yacc.yacc(debug = True) 
+#verificar que el archivo existe
+try:
+    namef = "/Users/ivananguiano/Desktop/HW3.2/prueba.txt"
+    file = open(namef,'r')
+    s = file.read()
+    file.close()
+except EOFError:
+    quit()
+#parsear archivo
+yacc.parse(s)
+print("Apropiado")
